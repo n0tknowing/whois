3a8482f1d51f3a55186275ec1a44719bd7ea5378
Widianto Nur Firmansah <xnaltasee@gmail.com>
2022-11-10 08:16:09 +0700

    whois: refactor
    changes:
    - remove '-v' option
    - new function whois_query() for querying

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 whois - simple whois command
 
   Usage:
-    whois [-hv] [-p PORT] [-s SERV] TLD
+    whois [-h] [-p PORT] [-s SERV] TLD
 
   Options:
     -4          Use IPv4 (default)
@@ -9,7 +9,7 @@ whois - simple whois command
     -h          Show help message
     -p PORT     Use port PORT instead of 43
     -s SERV     Use whois server SERV instead of whois.iana.org
-    -v          Verbose, show description each step
+
 
 installation and example
 ------------------------
@@ -26,32 +26,6 @@ organisation: Internet Assigned Numbers Authority
 created:      1992-01-01
 source:       IANA
 
-
-$ ./whois -v example.com
-=== WHOIS server whois.iana.org, port 43
-=== Querying example.com
-=== Calling getaddrinfo
-=== getaddrinfo OK (0x56051a650870)
-=== Creating socket descriptor
-=== Successfuly created socket descriptor (3)
-=== Start connecting to whois.iana.org, port 43
-=== Successfuly connected to whois.iana.org, port 43
-=== Start sending command "example.com\r\n"
-=== whois.iana.org response:
-
-% IANA WHOIS server
-% for more information on IANA, visit http://www.iana.org
-% This query returned 1 object
-
-domain:       EXAMPLE.COM
-
-organisation: Internet Assigned Numbers Authority
-
-created:      1992-01-01
-source:       IANA
-
-
-=== Received 233 bytes
 
 references
 ----------
--- whois.c
+++ whois.c
@@ -17,18 +17,82 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-/* completely ignore any non-ascii encoding */
-#define ISDIGIT(c) ((c) >= '0' && (c) <= '9')
+static void whois_query(const char *server, const char *port, int family,
+			const char *tldreq)
+{
+	char recvbuf[2048];
+	char *sendbuf;
+	size_t tldlen = strlen(tldreq);
+	ssize_t len;
+	int sockfd, rc;
+	struct addrinfo hint, *res;
+
+	memset(&hint, 0, sizeof(hint));
+	hint.ai_family = family;
+	hint.ai_socktype = SOCK_STREAM;
+
+	rc = getaddrinfo(server, port, &hint, &res);
+	if (rc < 0) {
+		const char *errmsg;
+		errmsg = (rc == EAI_SYSTEM) ? strerror(errno) : gai_strerror(rc);
+		fprintf(stderr, "getaddrinfo(): %s\n", errmsg);
+		exit(1);
+	}
+
+	sockfd = socket(res->ai_family, res->ai_socktype, 0);
+	if (sockfd < 0) {
+		fprintf(stderr, "socket(): %s\n", strerror(errno));
+		exit(1);
+	}
+
+	rc = connect(sockfd, res->ai_addr, res->ai_addrlen);
+	if (rc < 0) {
+		fprintf(stderr, "connect(): %s\n", strerror(errno));
+		exit(1);
+	}
+
+	sendbuf = calloc(tldlen + 3, sizeof(char));
+	if (sendbuf == NULL) {
+		fprintf(stderr, "malloc(): %s\n", strerror(errno));
+		exit(1);
+	}
+
+	memcpy(sendbuf, tldreq, tldlen);
+	sendbuf[tldlen] = '\r';
+	sendbuf[tldlen+1] = '\n';
+
+	len = send(sockfd, sendbuf, strlen(sendbuf), 0);
+	if (len < 1) {
+		const char *errmsg;
+		errmsg = (len < 0) ? strerror(errno) : "Empty";
+		fprintf(stderr, "send(): %s\n", errmsg);
+		exit(1);
+	}
+
+	while (1) {
+		len = recv(sockfd, recvbuf, sizeof(recvbuf), 0);
+		if (len < 1) break;
+		recvbuf[len] = '\0';
+		printf("%s", recvbuf);
+	}
+
+	freeaddrinfo(res);
+	free(sendbuf);
+	close(sockfd);
+}
 
 static int check_port(const char *port)
 {
 	const char *p = port;
 	int r = 0;
 
-	while (ISDIGIT(*p)) {
+	while (*p >= '0' && *p <= '9') {
 		r = r * 10;
+		if (r >= 65536)
+			break;
 		r = r + *p - '0';
-		if (r >= 65536) break;
+		if (r >= 65536)
+			break;
 		p++;
 	}
 
@@ -37,38 +101,28 @@ static int check_port(const char *port)
 
 static void usage(int exit_code)
 {
-	fprintf(stderr, "Usage:\n");
-	fprintf(stderr, "  whois [-hv] [-p PORT] [-s SERV] TLD\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "  -4          Use IPv4 (default)\n");
-	fprintf(stderr, "  -6          Use IPv6\n");
-	fprintf(stderr, "  -h          Show help message\n");
-	fprintf(stderr, "  -p PORT     Use port PORT instead of 43\n");
-	fprintf(stderr, "  -s SERV     Use whois server SERV instead of whois.iana.org\n");
-	fprintf(stderr, "  -v          Verbose, show description each step\n");
-
+	printf("Usage:\n");
+	printf("  whois [-h] [-p PORT] [-s SERV] TLD\n");
+	printf("\n");
+	printf("Options:\n");
+	printf("  -4          Use IPv4 (default)\n");
+	printf("  -6          Use IPv6\n");
+	printf("  -h          Show help message\n");
+	printf("  -p PORT     Use port PORT instead of 43\n");
+	printf("  -s SERV     Use whois server SERV instead of whois.iana.org\n");
 	exit(exit_code);
 }
 
 int main(int argc, char **argv)
 {
-	const char *prog = "whois";
-
-	int af = AF_INET;
-	char recv_buf[25], ip[INET6_ADDRSTRLEN];
-	int r = 0, fd = -1;
-	int opt, verbose = 0;
-	void *addr;
-	const char *port = "43";
-	ssize_t recv_r, recv_tot = 0;
-	const char *tld = NULL, *tld_tmp;
 	const char *server = "whois.iana.org";
-	struct addrinfo hint = {0}, *res = NULL;
+	const char *port = "43";
+	int opt, af = AF_INET;
 
-	while ((opt = getopt(argc, argv, "46hp:s:v")) != EOF) {
+	while ((opt = getopt(argc, argv, ":46hp:s:")) != EOF) {
 		switch (opt) {
 		case '4':
+			af = AF_INET;
 			break;
 		case '6':
 			af = AF_INET6;
@@ -78,16 +132,9 @@ int main(int argc, char **argv)
 			break;
 		case 'p':
 			port = optarg;
-			if (!check_port(port)) {
-				fprintf(stderr, "%s: invalid port %s\n", prog, port);
-				usage(1);
-			}
 			break;
 		case 's':
 			server = optarg;
-			break;
-		case 'v':
-			verbose = 1;
 			break;
 		default:
 			usage(1);
@@ -99,101 +146,17 @@ int main(int argc, char **argv)
 	argv += optind;
 
 	if (argc == 0) {
-		fprintf(stderr, "%s: TLD not specified\n", prog);
-		usage(1);
-	}
-
-	tld = argv[0];
-
-	if ((tld_tmp = strstr(tld, "//"))) {
-		if (verbose)
-			printf("=== Ignoring URI scheme on TLD\n");
-		tld_tmp += 2;
-		while (*tld_tmp == '/')
-			tld_tmp++;
-		tld = tld_tmp;
-	}
-
-	if (verbose) {
-		printf("=== WHOIS server %s, port %s\n", server, port);
-		printf("=== Querying %s\n", tld);
-	}
-
-	hint.ai_family = af;
-	hint.ai_socktype = SOCK_STREAM;
-
-	if (verbose)
-		printf("=== Calling getaddrinfo\n");
-
-	if ((r = getaddrinfo(server, port, &hint, &res)) < 0) {
-		if (r == EAI_SYSTEM)
-			err(1, "%s", server);
-		fprintf(stderr, "%s: %s\n", server, gai_strerror(r));
+		fprintf(stderr, "TLD not specified\n");
 		return 1;
 	}
 
-	if (verbose) {
-		printf("=== getaddrinfo OK (%p)\n", (void *)res);
-		printf("=== Creating socket descriptor\n");
-	}
-
-	if ((fd = socket(res->ai_family, res->ai_socktype, 0)) < 0) {
-		perror("Failed to created socket descriptor");
+	if (!check_port(port)) {
+		fprintf(stderr, "Invalid port '%s'\n", port);
 		return 1;
 	}
 
-	if (af == AF_INET)
-		addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-	else
-		addr = &((struct sockaddr_in6 *)res->ai_addr)->sin6_addr;
+	/* TODO: multiple tld requests? */
+	whois_query(server, port, af, argv[0]);
 
-	if (!inet_ntop(af, addr, ip, sizeof(ip)))
-		err(1, "inet_ntop");
-
-	if (verbose) {
-		printf("=== Successfuly created socket descriptor (fd=%d)\n", fd);
-		printf("=== Start connecting to %s (%s), port %s\n",
-				server, ip, port);
-	}
-
-	if ((r = connect(fd, res->ai_addr, res->ai_addrlen)) < 0) {
-		fprintf(stderr, "Failed to connecting to %s (%s), port %s: ",
-				server, ip, port);
-		perror(NULL);
-		return 1;
-	}
-
-	freeaddrinfo(res);
-
-	if (verbose) {
-		printf("=== Successfuly connected to %s (%s), port %s\n",
-				server, ip, port);
-		printf("=== Start sending command \"%s\\r\\n\"\n", tld);
-	}
-
-	if ((r = dprintf(fd, "%s\r\n", tld)) < 0) {
-		fprintf(stderr, "Failed to sending command \"%s\\r\\n\"", tld);
-		if (errno)
-			perror(NULL);
-		else
-			fprintf(stderr, "\n");
-		return 1;
-	}
-
-	if (verbose)
-		printf("=== %s response:\n\n", server);
-
-	while ((recv_r = recv(fd, recv_buf, 24, 0)) > 0) {
-		recv_buf[recv_r] = '\0';
-		printf("%s", recv_buf);
-		recv_tot += recv_r;
-	}
-
-	printf("\n");
-
-	if (verbose)
-		printf("=== Received %lu bytes\n", recv_tot);
-
-	close(fd);
 	return 0;
 }
